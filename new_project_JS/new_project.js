7ae81bd67105196ebee677decdbf4a6fe2b05cf6
mariohieu09 <trantrunghieu20092001@gmail.com>
2023-02-24 10:54:07 +0700

    su dung toan tu JS

---
+++ new_project_JS/new_project.js
@@ -0,0 +1,7 @@
+
+inputwidth = prompt("Enter the width");
+inputheight = prompt("Enter the height");
+let width = parseInt(inputwidth);
+let heigth = parseInt(inputheight);
+let area = heigth * width;
+document.write("The area is: " +area);
